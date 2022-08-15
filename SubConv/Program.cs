e3663afe6a0bd4d31030ac8b0ad67fc023579f10
kashelkin <svyateg@yandex.ru>
2022-08-15 17:00:09 +0300

    Add Help screen

--- SubConv/Program.cs
+++ SubConv/Program.cs
@@ -1,8 +1,11 @@
 ﻿using CommandLine;
+using CommandLine.Text;
 using SubConv;
 using SubConv.Providers.Ass;
 using SubConv.Providers.Srt;
 using SubConv.Transform;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -26,8 +29,11 @@ transformBuilder.RegisterFor("w",
     o => o.Count == 3,
     o => new WrapContentTransform(o[0], o[1], o[2]));
 
-Parser.Default.ParseArguments<Options>(args)
-    .WithParsed(o => Convert(o.InputPath, o.OutputPath, transformBuilder.Build(o.Transforms)));
+using var parser = new Parser(with => with.HelpWriter = null);
+var parserResult = parser.ParseArguments<Options>(args);
+parserResult
+    .WithParsed(options => Convert(options.InputPath, options.OutputPath, transformBuilder.Build(options.Transforms)))
+    .WithNotParsed(errs => Console.WriteLine(GetHelpText(parserResult, errs)));
 
 void Convert(string inputPath, string? outputPath, ISubtitleTransform transform)
 {
@@ -41,4 +47,43 @@ void Convert(string inputPath, string? outputPath, ISubtitleTransform transform)
 
         SrtWriter.Write(sw, transform.Transform(AssReader.Read(sr)));
     }
+}
+
+HelpText GetHelpText<T>(ParserResult<T> result, IEnumerable<Error> errs)
+{
+    if (errs.IsVersion())
+        return HelpText.AutoBuild(result);
+
+    return HelpText.AutoBuild(result, h =>
+    {
+        h.AddNewLineBetweenHelpSections = true;
+        h.AdditionalNewLineAfterOption = false;
+        h.MaximumDisplayWidth = 100;
+        h.AddPreOptionsLines(new[] {
+            "USAGE:",
+            "  subconv <InputPath>",
+            "  subconv <InputPath> -t [Transforms]",
+            "  subconv <InputPath> <OutputPath> -t [Transforms]",
+            "",
+            "TRANSFORMS:",
+            "  c                           Cleanup. Removes multiple spaces, empty lines and empty subtitle",
+            "                              entries.",
+            "  w:[Styles];[Start];[End]    Wrap. Wraps subtitle entries of certain styles in [Start] and [End]", 
+            "                              strings. If you want to wrap entries of all styles use *.",
+            "  m:[Styles1];[Styles2]...    Merge. Merges overlapping in time subtitle entries to single entry.",
+            "                              Vertical order of merged entries is specified in list of [Styles]. Merge must be the last transform because merged entries have no style information.",
+            "",
+            "EXAMPLES:",
+            "  subconv *.ass -t c w:Names,SmallNames;[;] w:smallfont;{;} m:Names,SmallNames;*;smallfont",
+            "",
+            "    - cleanups subtitle entries",
+            "    - wraps subtitle entries of styles 'Names' and 'SmallNames' in [];",
+            "    - wraps subtitle entries of style 'smallfont' in {};",
+            "    - merges subtitle entries. Vertical order:",
+            "          - 'Names', 'SmallNames'",
+            "          - all other styles",
+            "          - 'smallfont'."
+        });
+        return HelpText.DefaultParsingErrorsHandler(result, h);
+    }, e => e);
 }
